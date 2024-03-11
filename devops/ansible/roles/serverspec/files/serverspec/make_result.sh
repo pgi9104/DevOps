3cef58bdaf575c0eb54d0aa0e7a6bf87342ab086
park91s <park91s@naver.com>
2024-03-11 22:15:53 +0900

    [NFS][nfs-server 구성하기]

---
+++ devops/ansible/roles/serverspec/files/serverspec/make_result.sh
@@ -0,0 +1,2 @@
+rake spec SPEC_OPTS="--format html" > ~/result.html
+sudo mv ~/result.html /var/www/html/
---
+++ devops/ansible/roles/serverspec/templates/nfs_server_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe package('nfs-server') do
+  it { should be_installed }
+end
+
+describe service('nfs-server') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe file('/nfs-server') do
+  it { should be_directory }
+  it { should exist }
+end
